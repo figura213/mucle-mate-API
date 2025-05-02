10fd37f3cdd486c4cc0750f1ad2217c0fed420e4
figura213 <miskirov476@gmail.com>
2025-05-02 11:33:57 +0300

    first committt

--- src/main/java/com/rangers/demo/mapper/ExerciseMapper.java
+++ src/main/java/com/rangers/demo/mapper/ExerciseMapper.java
@@ -8,12 +8,8 @@ import org.mapstruct.*;
         unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ExerciseMapper {
 
-    @Mapping(target = "target", expression = "java(exercise.getTarget() != null ? exercise.getTarget().toArray(new String[0]) : null)")
+   // @Mapping(target = "target", expression = "java(exercise.getTarget() != null ? exercise.getTarget().toArray(new String[0]) : null)")
     ExerciseDto toDto(Exercise exercise);
 
-    @Mapping(target = "target", expression = "java(exerciseDto.getTarget() != null ? java.util.Arrays.asList(exerciseDto.getTarget()) : null)")
-    @Mapping(target = "equipment", ignore = true)
-    @Mapping(target = "mediaLinks", ignore = true)
-    @Mapping(target = "tags", ignore = true)
     Exercise toEntity(ExerciseDto exerciseDto);
 }
